f85f99d1011ee7fea5a0576b9a895badb00e575d
Kabileshwaran183 <Kabileshwaran183@gmail.com>
2025-01-09 13:28:00 +0530

    created the sections of our proj

--- src/App.jsx
+++ src/App.jsx
@@ -16,13 +16,13 @@ const App = () => (
     <section className="padding">
       SpecialOffers
     </section>
-    <section className="padding">
+    <section className="padding bg-pale-blue">
       CustomerReviews
     </section>
-    <section className="padding">
+    <section className="padding-x sm:py-32 py-16 w-full">
       Subscribe
     </section>
-    <section className="padding">
+    <section className="bg-black padding-x padding-t pb-8">
       Footer
     </section>
 
