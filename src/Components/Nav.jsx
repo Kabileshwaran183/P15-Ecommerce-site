18d7221aff982a3a495ad7a26de3ed2e689539a2
Kabileshwaran183 <Kabileshwaran183@gmail.com>
2025-01-09 14:26:51 +0530

    Navbar Section , also add all the constants in index.js

--- src/Components/Nav.jsx
+++ src/Components/Nav.jsx
@@ -1,8 +1,35 @@
-
+import logo from '../assets/images/header-logo.svg'
+import { hamburger } from '../assets/icons'
+import { navLinks } from '../Constants/index.js'
 const Nav = () => {
-  return (
-    <div>Nav</div>
-  )
+    return (
+        <header className='padding-x py-8 absolute z-10 w-full'>
+            <nav className='flex justify-between items-center max-container'>
+                <a href="./">
+                    <img src={logo}
+                        alt="logo-img"
+                        width={130}
+                        height={30} />
+                </a>
+                <ul className='flex-1 flex justify-center items-center gap-16 max-lg:hidden'>
+                    {navLinks.map((item) =>(
+                        <li key={item.label}>
+                            <a href={item.href}
+                                className='font-montserrat text-lg leading-normal text-slate-gray'>
+                                {item.label}
+                            </a>
+                        </li>
+                    ))}
+                </ul>
+                <div className='hidden max-lg:block'>
+                    <img src={hamburger}
+                        alt="hamburger-icon"
+                        width={24}
+                        height={24} />
+                </div>
+            </nav>
+        </header>
+    )
 }
 
 export default Nav
