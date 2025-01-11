bc92bc7eb5fa9fad8ab2aafb6917f02e40282ae3
Kabileshwaran183 <Kabileshwaran183@gmail.com>
2025-01-11 08:55:00 +0530

    Revie section

--- src/Sections/CustomerReviews.jsx
+++ src/Sections/CustomerReviews.jsx
@@ -1,7 +1,27 @@
+import ReviewCard from "../Components/ReviewCard"
+import { reviews } from "../Constants"
 
 const CustomerReviews = () => {
   return (
-    <div>CustomerReviews</div>
+    <section className="max-container"> 
+      <h2 className="font-palanquin font-bold text-center text-4xl">
+        What Our 
+        <span className="text-coral-red"> Customers </span>
+        Say ?
+      </h2>
+      <p className='m-auto mt-4 max-w-lg  text-center info-text'>
+        Hear genuine stories from our satisfied customers about their
+        exceptional experiences with us.
+      </p>
+      <div className="flex flex-1 justify-evenly mt-24 items-center max-lg:flex-col gap-14">
+          {reviews.map((review)=>(
+            <ReviewCard 
+                  key={review.customerName}
+                  {...review}/>
+          ))}
+      </div>
+
+    </section>
   )
 }
 
